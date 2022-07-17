b0042aa66045ccd30fe7a38f15325629f29aca7f
BojanaCakarevic <bojanacakarevicc@gmail.com>
2022-07-17 18:51:19 +0200

    Primeri sa cetvrtog predavanja

--- stock-market/src/app/stock/stock-item/stock-item.component.ts
+++ stock-market/src/app/stock/stock-item/stock-item.component.ts
@@ -10,23 +10,56 @@ import { Stock } from 'src/app/model/stock';
 export class StockItemComponent implements OnInit {
 
   public stock!: Stock;
+  public stock1!: Stock;
+  public stock2!: Stock;
+  public stockClasses;
+
+  public stocks!: Array<Stock>
 
   constructor() {
     this.stock = {
       name: "Test Stock Company",
       code: "TSC",
-      price: 80,
-      previousPrice: 85,
+      price: 180,
+      previousPrice: 285,
       favorite: false
     }
-   }
+    this.stock1 = {
+      name: "Second Stock Company",
+      code: "SSC",
+      price: 18,
+      previousPrice: 5,
+      favorite: false
+    }
+    this.stock2 = {
+      name: "Last Stock Company",
+      code: "LSC",
+      price: 80,
+      previousPrice: 285,
+      favorite: false
+    }
+
+    this.stocks = [];
+    this.stocks.push(this.stock1);
+    this.stocks.push(this.stock2);
+    this.stocks.push(this.stock);
+
+    let diff = (this.stock.price / this.stock.previousPrice) - 1;
+    let largeChange = Math.abs(diff) > 0.01;
+    this.stockClasses = {
+      "positive": this.isPositiveChange(),
+      "negative": !this.isPositiveChange(),
+      "large-change": largeChange,
+      "small-change": !largeChange
+    };
+  }
 
   ngOnInit(): void {
   }
 
-  toggleFavorite() {
-    console.log("We are toogling the favorite state for this stock");
-    this.stock.favorite = !this.stock.favorite;
+  toggleFavorite(index:number) {
+    console.log("We are toogling the favorite state for this stock", index);
+    this.stocks[index].favorite = !this.stock.favorite;
   }
 
   isPositiveChange(): boolean {
